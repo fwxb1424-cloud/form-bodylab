21b0b53f23ddf1a9fc68b7036447d90ea6ab0cc6
fwxb1424-cloud <fwxb1424@gmail.com>
2026-05-18 00:21:37 +0900

    Delete generate-icons.js

--- generate-icons.js
+++
@@ -1,5 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" viewBox="0 0 192 192">
-  <rect width="192" height="192" fill="#0e0e0f"/>
-  <text x="96" y="96" font-family="Arial Black, sans-serif" font-weight="900" 
-        font-size="96" fill="#c8f060" text-anchor="middle" dominant-baseline="central">F</text>
-</svg>
